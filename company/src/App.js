93cebd6a105635c64c79a7e2b50d54a9e73e113e
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-05-24 21:11:48 +0900

    404 페이지 구현

--- company/src/App.js
+++ company/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';  // Navbar 컴포넌트 불러오기
 import Home from './Home';
 import Introduction from './Introduction';
+import NotFound from './NotFound';
 // 다른 컴포넌트도 필요에 따라 불러옵니다.
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/introduction" element={<Introduction />} />
+        <Route path='*' element={<NotFound/>}/>
         {/* 여기에 더 많은 라우트를 추가할 수 있습니다 */}
       </Routes>
     </BrowserRouter>
