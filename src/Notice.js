c206a805a956d137b760090aa023fc8d8b5971f0
jihyun132 <jihyun.q.s@gmail.com>
2024-05-26 11:51:48 +0900

    post 기본적인 기능

--- src/Notice.js
+++ src/Notice.js
@@ -1,9 +1,27 @@
+import { useState } from "react"
+
 const Notice = () => {
+
+    const [posts,setPosts] = useState(['hi','hello'])
+
+    const createNotice = () => {
+        const newPost = document.getElementById('post').value
+        setPosts([...posts, newPost])
+        console.log(posts)
+    }
 
     return(
         <div>
             <h2>Notice Page</h2>
-            
+            <div>
+                <input type="text" placeholder="내용입력" id="post"></input>
+                <input type="button" value="create" onClick={createNotice}></input>
+                <ul>
+                    {posts.map((e)=>
+                    <li className="post">{e}</li>
+                    )}
+                </ul>
+            </div>
         </div>
     )
 }
