fb1045660b8e456d512e89e13ff7523660b5cfb7
jihyun132 <jihyun.q.s@gmail.com>
2024-05-27 15:17:06 +0900

    Notice 생성 및 리스트 추가

--- src/Notice.js
+++ src/Notice.js
@@ -1,10 +1,21 @@
+import { Outlet, useNavigate } from "react-router-dom";
+
 const Notice = () => {
+
+    const navigate = useNavigate();
     
+    const MoveToNoticeCreate = (e) => {
+        e.preventDefault();
+        navigate("/notice/write");
+    }
 
     return(
         <div>
             <h2>Notice Page</h2>
-            
+            <div>
+                <button onClick={MoveToNoticeCreate}>write</button>
+            </div>
+            <Outlet/>
         </div>
     )
 }
