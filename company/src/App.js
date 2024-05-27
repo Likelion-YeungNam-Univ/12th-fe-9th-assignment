8591bdb5f20a86254a5e73873b22dcc290f2a5a7
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-05-28 00:56:28 +0900

    New Arrivals 컴포넌트 Best Product 수정 및 NavBar 수정

--- company/src/App.js
+++ company/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';  // Navbar 컴포넌트 불러오기
+import Navbar from './components/Navbar';  // Navbar 컴포넌트 불러오기
 import Home from './Home';
 import Introduction from './Introduction';
 import NotFound from './NotFound';
+import BestProduct from './components/BestProduct';
 // 다른 컴포넌트도 필요에 따라 불러옵니다.
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
     <BrowserRouter>
       <Navbar />  
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/home" element={<Home />} />
         <Route path='*' element={<NotFound/>}/>
+        <Route path='/products/:id' element={<BestProduct></BestProduct>}></Route>
         {/* 여기에 더 많은 라우트를 추가할 수 있습니다 */}
       </Routes>
     </BrowserRouter>
--- company/src/Home.jsx
+++ company/src/Home.jsx
@@ -1,6 +1,29 @@
-function Home(){
-    return (
-        <div></div>
-    )
+import styled from "styled-components";
+import NewArrivals from "./components/NewArrivals";
+import BestProduct from "./components/BestProduct";
+import { useNavigate } from "react-router-dom";
+const Img = styled.img`
+  margin-top: 60px;
+  width: 100%;
+  overflow-x: hidden;
+  height: auto;
+
+`;
+const SizedBox=styled.div`
+  width: 100%;
+  background-color: white;
+  height: 100px;
+`
+function Home() {
+  
+  return (
+    <>
+    {/* 홈 로고 클릭 시 메인화면 이동 */}
+      <Img src="/img/background.png" ></Img>
+      <NewArrivals></NewArrivals>
+      <SizedBox/>
+      <BestProduct />
+    </>
+  );
 }
 export default Home;
