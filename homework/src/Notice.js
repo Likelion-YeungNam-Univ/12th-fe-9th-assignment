cb2ce9e4ef31fcdb4c68c6953648c4d9754baae2
Yeongi Park <yeongi0111@naver.com>
2024-05-27 21:32:40 +0900

    공지사항 초기 데이터 저장

--- homework/src/Notice.js
+++ homework/src/Notice.js
@@ -1,6 +1,23 @@
 import React from "react";
 
 export const Notice = () => {
+  const noticeList = [
+    {
+      id: 1,
+      title: "공지사항1",
+      content: "안녕하세요 공지사항1입니다.",
+      writer: "운영자",
+    },
+    {
+      id: 2,
+      title: "공지사항2",
+      content: "안녕하세요 공지사항2입니다.",
+      writer: "운영자",
+    },
+  ];
+
+  localStorage.setItem("noticeList", JSON.stringify(noticeList));
+
   return (
     <div>
       <h1>공지사항</h1>
