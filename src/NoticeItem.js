894c091733b0a35e81130b04ecd61970ce8e2052
jihyun132 <jihyun.q.s@gmail.com>
2024-05-28 11:12:26 +0900

    read기능 > modify 기능 추가 및 변경

--- src/NoticeItem.js
+++ src/NoticeItem.js
@@ -3,18 +3,41 @@ import { useState } from "react";
 const NoticeItem = ({notice}) => {
 
     const [isRead, setIsRead] = useState(false);
+    const [isModified, setIsModified] = useState(false);
+    const [titleUpdate, setTitleUpdate] = useState(notice.title);
+    const [contentUpdate, setContentUpdate] = useState(notice.content);
+
+        
+    const newtitle = (e) => {
+        setTitleUpdate(e.target.value)
+    }
+    const newcontent = (e) => {
+        setContentUpdate(e.target.value)
+    }
 
     return(
         <div>
             {isRead?(
                 <div>
-                    <h2>{notice.title}</h2>
-                    <p>{notice.content}</p>
-                    <button onClick={()=>setIsRead(false)}>back</button>
+                    {isModified?(
+                        <>
+                            <input type="text" onChange={newtitle} value={titleUpdate}></input>
+                            <input type="text" onChange={newcontent} value={contentUpdate}></input>
+                            <button onClick={()=>setIsModified(false)}>post</button>
+                        </>
+                    ):(
+                        <>
+                            <h2>{titleUpdate}</h2>
+                            <p>{contentUpdate}</p>
+                            <button onClick={()=>setIsModified(true)}>modify</button>
+                            <button onClick={()=>setIsRead(false)}>back</button>
+                        
+                        </>
+                )}
                 </div>
             ):(
                 <div>
-                    <p>{notice.title}</p>
+                    <p>{titleUpdate}</p>
                     <button onClick={()=>setIsRead(true)}>read</button>
                 </div>
             )}
